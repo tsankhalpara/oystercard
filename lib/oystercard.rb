f7a061a40d27f2ecfc169c31efaa3ed3a0efabf2
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-10 14:04:18 +0100

    6. Enforcing maximum balance: Raise error when the balance exceeds the max balance of £90.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,14 @@ class Oystercard
 
   attr_reader :balance
 
+  MAX_BALANCE = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Exceeded limit of #{MAX_BALANCE}" if @balance >= MAX_BALANCE
     @balance += amount
     return "Your card has a balance of £#{@balance}"
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,6 +10,14 @@ describe Oystercard do
     it 'increases the balance' do
       expect { subject.top_up(1) }.to change { subject.balance }.by(1)
     end
+
+    context "Exceeded limit" do
+      it "raises error" do
+        max = Oystercard::MAX_BALANCE
+        subject.top_up(max)
+        expect { subject.top_up 1 }.to raise_error "Exceeded limit of #{max}"
+      end
+    end
   end
 
 end
