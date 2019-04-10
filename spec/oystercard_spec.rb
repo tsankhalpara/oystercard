6252a0ef92b743842fa8f99fa60bcf08fd0d73ca
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-10 13:21:31 +0100

    5. Enable top-up functionality, allow balance to be updated by a specified amount

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,12 @@ describe Oystercard do
 
   it { is_expected.to respond_to(:balance)}
 
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument}
+
+    it 'increases the balance' do
+      expect { subject.top_up(1) }.to change { subject.balance }.by(1)
+    end
+  end
+
 end
