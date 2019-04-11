4990dca078abe1637f64508662336abe14676c84
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-11 09:36:40 +0100

    added definitions and expected results from touch in/out and in_journey?

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,11 +2,25 @@ require 'journey'
 
 describe Journey do
 
+  it { is_expected.to respond_to(:in_journey)}
+
+  describe '#initialize' do
+    it "Assumes user starts off not in a journey" do
+      expect(subject.in_journey).to eq false
+    end
+  end
+
   describe '#touch_in' do
-    
+    it "will change in journey state to true" do
+      subject.touch_in
+      expect(subject.touch_in).to eq true
+    end
   end
 
   describe '#touch_out' do
-
+    it "will change in journey state to false" do
+      subject.touch_out
+      expect(subject.touch_out).to eq false
+    end
   end
 end
