7483f0de26d888ae5b93d9804db002a4cbf3a9ed
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-11 10:24:56 +0100

    move all tests and definitions from journey class back into oystercard.rb for now - will focus on refactoring and seperate classes later

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,14 @@ require 'oystercard'
 describe Oystercard do
 
   it { is_expected.to respond_to(:balance)}
+  it { is_expected.to respond_to(:in_journey)}
 
+  describe '#initialize' do
+    it "Assumes user starts off not in a journey" do
+      expect(subject.in_journey).to eq false
+    end
+  end
+  
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument}
 
@@ -23,6 +30,20 @@ describe Oystercard do
   describe '#deduct' do
     it "decreases the balance" do
       expect { subject.deduct(1) }.to change { subject.balance }.by(-1)
+    end
+  end
+
+  describe '#touch_in' do
+    it "will change in journey state to true" do
+      subject.touch_in
+      expect(subject.touch_in).to eq true
+    end
+  end
+
+  describe '#touch_out' do
+    it "will change in journey state to false" do
+      subject.touch_out
+      expect(subject.touch_out).to eq false
     end
   end
 
